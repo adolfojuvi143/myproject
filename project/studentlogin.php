44bbf37b6fd98816ff100d491711e4b9e756dbce
adolfojuvi143 <58077394+adolfojuvi143@users.noreply.github.com>
2019-11-27 01:20:48 -0800

    Add files via upload

---
+++ project/studentlogin.php
@@ -0,0 +1,35 @@
+<?php include('server.php') ?>
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Registration system PHP and MySQL</title>
+  <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<style type="text/css">
+  body{
+    background-image: url(bet.jpg);
+    background-attachment:fixed;
+    background-size:cover;
+   
+
+  }
+  form, .content {
+    width: 30%;
+    margin: 0px auto;
+    padding: 20px;
+    border: 1px solid #b0c4de40;
+    background: #ffffff80;
+    border-radius: 0px 0px 10px 10px;
+}
+</style>
+<body>
+  <div class="header">
+  <br>
+  <h2 style="color:red;">Log in as</h2>
+    <a href="login1.php"><h2 style="color:gray;">Bet User</h2></a>
+    <a href="login.php"><h2 style="color:gray;">Admin</h2><a>
+
+  </div>
+   
+</body>
+</html>
